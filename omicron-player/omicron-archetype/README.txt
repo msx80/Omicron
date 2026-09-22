2985a16bde7da7bb3f8c1d2b15ef17e71c41adb0
msx <msx@example.com>
2026-09-22 16:32:39 +0200

    Archetype upgrade and improvements [no ci]

--- omicron-player/omicron-archetype/README.txt
+++ omicron-player/omicron-archetype/README.txt
@@ -1,1 +1,8 @@
-mvn archetype:generate -DarchetypeGroupId=com.github.msx80.omicron  -DarchetypeArtifactId=omicron-archetype -DarchetypeVersion=0.0.11
+Command to generate project:
+
+mvn archetype:generate -DarchetypeGroupId=com.github.msx80.omicron  -DarchetypeArtifactId=omicron-archetype -DarchetypeVersion=0.0.13-SNAPSHOT
+
+Then go inside the folder and run:
+
+mvn clean compile exec:exec
+
--- omicron-player/omicron-archetype/src/main/resources/archetype-resources/src/main/java/__classPrefix__.java
+++ omicron-player/omicron-archetype/src/main/resources/archetype-resources/src/main/java/__classPrefix__.java
@@ -8,30 +8,50 @@ import com.github.msx80.omicron.api.Game;
 import com.github.msx80.omicron.api.Sys;
 import com.github.msx80.omicron.api.SysConfig;
 import com.github.msx80.omicron.api.SysConfig.VirtualScreenMode;
+import com.github.msx80.omicron.basicutils.text.TextDrawer;
+import com.github.msx80.omicron.basicutils.text.TextDrawerVariable;
+import com.github.msx80.omicron.api.Controller;
+import com.github.msx80.omicron.basicutils.Colors;
 
 public class ${classPrefix} implements Game {
-	
-	public static final int HEIGHT = 144;
-	public static final int WIDTH = 256;
-	
-    public void init() 
-    {
-    	
-    }
+
+  public static final int HEIGHT = 144;
+  public static final int WIDTH = 256;
+
+  private TextDrawer textDrawer;
+  private int x = 50;
+  private int y = 50;
+
+  public void init() 
+  {
+    textDrawer = TextDrawerVariable.DEFAULT;
+  }
 
 	public boolean loop() 
 	{
-		Sys.clear(255); // black
-		Sys.fill(0,10,10,100,100,-16776961);  // red
+	  Controller c = Sys.controllers()[0];
+	  
+	  if(c.up()) y--;
+	  if(c.down()) y++;
+	  if(c.left()) x--;
+	  if(c.right()) x++;
+	  
+	  // rendering
+	  
+    Sys.clear( Colors.from(30, 60, 90) );
+    Sys.fill( 0,5,5,WIDTH-10,20, Colors.RED ); 
+    textDrawer.print("Hello world! Move the robot with cursor keys :)", 10, 10);
+    
+    // cute robot sprite by https://wrabitart.itch.io/
+    Sys.draw(1, x, y, 0, 0, 16, 16,  0, 0);
+    
+    return true;
+  }
 
-        return true;
-    }
-
-
-	@Override
-	public SysConfig sysConfig() 
-	{
-		return new SysConfig(WIDTH, HEIGHT, VirtualScreenMode.FILL_SIDE, "${classPrefix}", "${artifactId}");
-	}
+  @Override
+  public SysConfig sysConfig() 
+  {
+    return new SysConfig(WIDTH, HEIGHT, VirtualScreenMode.FILL_SIDE, "${classPrefix}", "${artifactId}");
+  }
 
 }
